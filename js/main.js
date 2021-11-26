c8893377ead96ec541641f4b676a074c179c0623
KimByungHun <k01087831451@gmail.com>
2021-11-26 23:25:09 +0900

    js ripple effect

--- js/main.js
+++ js/main.js
@@ -18,13 +18,6 @@ btn.onmousemove = function(e){
 }
 const btn2 = document.querySelector(".black2");
 
-btn2.onmousemove = function(e){
-    const x = e.pageX - e.target.offsetLeft;
-    const y = e.pageY - e.target.offsetTop;
-
-    btn2.style.setProperty('--x',x + 'px');
-    btn2.style.setProperty('--y',y + 'px');
-}
 
 // const btns = document.querySelectorAll(".black");
 // btns.forEach(btn => {
